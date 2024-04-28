780840942acbf104253f0a1686b64b6324ea158e
Viacheslav Kondratiuk <viacheslav.kondratiuk@2xideas.com>
2024-04-28 14:46:03 +0300

    pong

--- app/main.py
+++ app/main.py
@@ -1,15 +1,18 @@
-# Uncomment this to pass the first stage
 import socket
 
 
-def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
+def main():    
     print("Logs from your program will appear here!")
 
-    # Uncomment this to pass the first stage
+    pong = "+PONG\r\n"
     
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    conn, addr = server_socket.accept()
+
+    with conn:
+        conn.recv(1024)
+        conn.send(pong.encode())
+    
 
 
 if __name__ == "__main__":
