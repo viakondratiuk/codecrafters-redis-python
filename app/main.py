dfb831a91117f409405e3551401504fb3beafba1
Viacheslav Kondratiuk <viacheslav.kondratiuk@2xideas.com>
2024-04-28 15:20:44 +0300

    pong pong

--- app/main.py
+++ app/main.py
@@ -9,11 +9,15 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     conn, addr = server_socket.accept()
 
-    with conn:
-        conn.recv(1024)
-        conn.send(pong.encode())
+    while True:
+        try:
+            data = conn.recv(1024)
+            conn.send(pong.encode())
+        except Exception as e:
+            print(f"Got error: {e}")        
     
-
+    # conn.close()
+    # server_socket.close()
 
 if __name__ == "__main__":
     main()
