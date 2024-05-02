8e043d58feb2626bb8ff87f6bef984c297c701cf
Viacheslav Kondratiuk <viacheslav.kondratiuk@2xideas.com>
2024-05-02 20:23:09 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,6 +1,48 @@
 import asyncio
+from enum import Enum
 
-PONG = b"+PONG\r\n"
+
+PONG = "+PONG\r\n"
+
+
+class Byte(Enum):
+    ARRAY = "*"
+    STRING = "+"
+
+
+class Command(str, Enum):
+    PING = "ping"
+    ECHO = "echo"
+
+
+# *2\r\n$4\r\necho\r\n$3\r\nhey\r\n
+# echo -ne "*2\r\n$4\r\necho\r\n$3\r\nhey\r\n" | nc localhost 6379
+class RedisProtocol:
+    def parse(self, request: str):
+        return request.split("\r\n")
+    
+
+class Handlers:
+    @staticmethod
+    def ping(args):
+        return PONG
+    
+    @staticmethod
+    def echo(args):
+        response = []
+        
+        for arg in args:
+            response.append(f"${len(arg)}")
+            response.append(arg)
+        response.append("")
+
+        return "\r\n".join(response)
+    
+
+HANDLERS_MAP = {
+    Command.PING: Handlers.ping,
+    Command.ECHO: Handlers.echo,
+}
 
 
 async def main():    
@@ -11,12 +53,23 @@ async def main():
     
 
 async def handle_client(client_reader, client_writer):
-    while True:
-        data = await client_reader.read(1024)
-        if not data:
-            break
-        client_writer.write(PONG)
+    redis_protocol = RedisProtocol()
+    
+    while request := await client_reader.read(1024):
+        
+        args = redis_protocol.parse(request.decode("utf-8"))
+        cmd = args[2].lower()
+        response = ""
+        match cmd:
+            case "ping":
+                response = PONG
+            case "echo":
+                response = Handlers.echo([args[4]])
+
+        client_writer.write(response.encode())
+        
         await client_writer.drain()
+    
     client_writer.close()
 
 
